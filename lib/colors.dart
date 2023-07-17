7ea80543abb2385a6ab99f2ddb599e1f8136b66d
Ben George Netto <bengnetto245@gmail.com>
2023-07-17 17:54:38 +0530

    add theme

---
+++ lib/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color voidColor = Color(0xFF19151e);
+  static const Color manga = Color(0xFFe7e2e7);
+  static const Color spark = Color(0xFFfce27e);
+  static const Color surf = Color(0xFF79d2f0);
+  static const Color psybeam = Color(0xFF8b8dc5);
+  static const Color giga = Color(0xFFb9d432);
+  static const Color blueGray = Color(0xFF262a35);
+  static const Color buttonBlue = Color(0xFF2367ff);
+}
---
+++ lib/theme.dart
@@ -0,0 +1,84 @@
+import 'package:collevo_teacher/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomTheme {
+  static ThemeData getThemeData() {
+    return ThemeData(
+      primaryColor: CustomColors.manga,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+          fontFamily: 'Poppins',
+          fontWeight: FontWeight.w500,
+          fontSize: 28,
+        ),
+      ),
+      textTheme: GoogleFonts.poppinsTextTheme(),
+      scaffoldBackgroundColor: CustomColors.voidColor,
+      cardTheme: const CardTheme(
+        color: CustomColors.blueGray,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(16.0),
+          ),
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: CustomColors.buttonBlue,
+          padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.2),
+          ),
+          textStyle: const TextStyle(
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w600,
+            fontSize: 18,
+          ),
+        ),
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+            foregroundColor: CustomColors.surf,
+            textStyle: const TextStyle(
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w500,
+              fontSize: 14,
+            )),
+      ),
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: OutlinedButton.styleFrom(
+          // foregroundColor: CustomColors.buttonBlue,
+          // backgroundColor: CustomColors.voidColor,
+          padding: const EdgeInsets.symmetric(
+            vertical: 12.0,
+            horizontal: 24.0,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          textStyle: const TextStyle(
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w500,
+            fontSize: 14,
+          ),
+        ),
+      ),
+      colorScheme: ColorScheme.fromSwatch(
+        brightness: Brightness.dark,
+        backgroundColor: CustomColors.voidColor,
+      ).copyWith(surface: CustomColors.blueGray),
+      dialogTheme: const DialogTheme(
+        backgroundColor: CustomColors.blueGray,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(16.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
