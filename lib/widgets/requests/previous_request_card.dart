ec64f0abca3943a9731b18a11db21a0420b9c842
Ben George Netto <bengnetto245@gmail.com>
2024-02-28 16:12:05 +0530

    Make request cards responsive

--- lib/widgets/requests/previous_request_card.dart
+++ lib/widgets/requests/previous_request_card.dart
@@ -15,41 +15,52 @@ class PreviousRequestCard extends StatelessWidget {
     double sizedBoxSize = MediaQuery.of(context).size.height * 0.05 / 4;
     double imageSize =
         preferredSize > imageMaxSize ? imageMaxSize : preferredSize;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: Card(
+    preferredSize > imageMaxSize ? imageMaxSize : preferredSize;
+    double screenWidth = MediaQuery.of(context).size.width;
+    double desiredWidth = screenWidth * 0.8;
+    double maxWidth = 1400;
+    double containerWidth = desiredWidth > maxWidth ? maxWidth : desiredWidth;
+    return Align(
+      alignment: Alignment.center,
+      child: SizedBox(
+        width: containerWidth,
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: Text(
-                  'Request Date: ${request.createdAt.day.toString().padLeft(2, '0')}-${request.createdAt.month.toString().padLeft(2, '0')}-${request.createdAt.year}',
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              SizedBox(height: sizedBoxSize),
-              SizedBox(
-                width: double.infinity,
-                child: Center(
-                  child: SizedBox(
-                    width: imageSize,
-                    child: Image.network(
-                      request.imageUrl,
-                      fit: BoxFit.fitWidth,
-                      errorBuilder: (context, error, stackTrace) {
-                        return const Icon(Icons.error);
-                      },
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Text(
+                      'Request Date: ${request.createdAt.day.toString().padLeft(2, '0')}-${request.createdAt.month.toString().padLeft(2, '0')}-${request.createdAt.year}',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
                   ),
-                ),
+                  SizedBox(height: sizedBoxSize),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Center(
+                      child: SizedBox(
+                        width: imageSize,
+                        child: Image.network(
+                          request.imageUrl,
+                          fit: BoxFit.fitWidth,
+                          errorBuilder: (context, error, stackTrace) {
+                            return const Icon(Icons.error);
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
--- lib/widgets/requests/request_card.dart
+++ lib/widgets/requests/request_card.dart
@@ -13,125 +13,135 @@ class RequestCard extends StatelessWidget {
     var requestIdSplit = request.requestId.split('_');
     // final requestIdSubstring = requestIdSplit[2];
     final studentName = requestIdSplit[1];
-    double imageMaxSize = 800;
-    double preferredSize = MediaQuery.of(context).size.width * 0.5;
+    double imageMaxSize = 900;
+    double preferredSize = MediaQuery.of(context).size.width * 0.7;
     double imageSize =
         preferredSize > imageMaxSize ? imageMaxSize : preferredSize;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: Card(
-        child: ExpansionTile(
-          title: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-                '$studentName: ${request.activity} - ${request.activityLevel}'),
-          ),
-          // subtitle: Padding(
-          //   padding: const EdgeInsets.all(8.0),
-          //   child: Column(
-          //     crossAxisAlignment: CrossAxisAlignment.start,
-          //     children: [
-          //       Text(
-          //         'Request ID:',
-          //         style: Theme.of(context).textTheme.bodyMedium,
-          //       ),
-          //       Text(
-          //         requestIdSubstring,
-          //         style: Theme.of(context).textTheme.labelSmall,
-          //       ),
-          //     ],
-          //   ),
-          // ),
-          collapsedShape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16),
-          ),
-          backgroundColor: Colors.black87,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: double.infinity,
-                    child: Center(
-                      child: SizedBox(
-                        width: imageSize,
-                        child: Image.network(
-                          request.imageUrl,
-                          fit: BoxFit.fitWidth,
-                          errorBuilder: (context, error, stackTrace) {
-                            // print(stackTrace);
-                            // print(error);
-                            return const Icon(Icons.error);
-                          },
+    double screenWidth = MediaQuery.of(context).size.width;
+    double desiredWidth = screenWidth * 0.8;
+    double maxWidth = 1400;
+    double containerWidth = desiredWidth > maxWidth ? maxWidth : desiredWidth;
+    return Align(
+      alignment: Alignment.center,
+      child: SizedBox(
+        width: containerWidth,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: Card(
+            child: ExpansionTile(
+              title: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                    '$studentName: ${request.activity} - ${request.activityLevel}'),
+              ),
+              // subtitle: Padding(
+              //   padding: const EdgeInsets.all(8.0),
+              //   child: Column(
+              //     crossAxisAlignment: CrossAxisAlignment.start,
+              //     children: [
+              //       Text(
+              //         'Request ID:',
+              //         style: Theme.of(context).textTheme.bodyMedium,
+              //       ),
+              //       Text(
+              //         requestIdSubstring,
+              //         style: Theme.of(context).textTheme.labelSmall,
+              //       ),
+              //     ],
+              //   ),
+              // ),
+              collapsedShape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              backgroundColor: Colors.black87,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        child: Center(
+                          child: SizedBox(
+                            width: imageSize,
+                            child: Image.network(
+                              request.imageUrl,
+                              fit: BoxFit.fitWidth,
+                              errorBuilder: (context, error, stackTrace) {
+                                // print(stackTrace);
+                                // print(error);
+                                return const Icon(Icons.error);
+                              },
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Created at:',
-                            style: Theme.of(context).textTheme.bodyMedium,
+                      const SizedBox(height: 8),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Created at:',
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                              Text(
+                                '${request.createdAt.day.toString().padLeft(2, '0')}:${request.createdAt.month.toString().padLeft(2, '0')}:${request.createdAt.year}    ${request.createdAt.hour.toString().padLeft(2, '0')}:${request.createdAt.minute.toString().padLeft(2, '0')}',
+                                style: Theme.of(context).textTheme.labelSmall,
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                'Activity Type:',
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                              Text(
+                                request.activityType,
+                                style: Theme.of(context).textTheme.labelSmall,
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                'Activity:',
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                              Text(
+                                request.activity,
+                                style: Theme.of(context).textTheme.labelSmall,
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                'Activity Level:',
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                              Text(
+                                request.activityLevel,
+                                style: Theme.of(context).textTheme.labelSmall,
+                              ),
+                              const SizedBox(height: 8),
+                              // Text(
+                              //   'Activity ID:',
+                              //   style: Theme.of(context).textTheme.bodyMedium,
+                              // ),
+                              // Text(
+                              //   request.activityId,
+                              //   style: Theme.of(context).textTheme.labelSmall,
+                              // ),
+                            ],
                           ),
-                          Text(
-                            '${request.createdAt.day.toString().padLeft(2, '0')}:${request.createdAt.month.toString().padLeft(2, '0')}:${request.createdAt.year}    ${request.createdAt.hour.toString().padLeft(2, '0')}:${request.createdAt.minute.toString().padLeft(2, '0')}',
-                            style: Theme.of(context).textTheme.labelSmall,
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            'Activity Type:',
-                            style: Theme.of(context).textTheme.bodyMedium,
-                          ),
-                          Text(
-                            request.activityType,
-                            style: Theme.of(context).textTheme.labelSmall,
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            'Activity:',
-                            style: Theme.of(context).textTheme.bodyMedium,
-                          ),
-                          Text(
-                            request.activity,
-                            style: Theme.of(context).textTheme.labelSmall,
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            'Activity Level:',
-                            style: Theme.of(context).textTheme.bodyMedium,
-                          ),
-                          Text(
-                            request.activityLevel,
-                            style: Theme.of(context).textTheme.labelSmall,
-                          ),
-                          const SizedBox(height: 8),
-                          // Text(
-                          //   'Activity ID:',
-                          //   style: Theme.of(context).textTheme.bodyMedium,
-                          // ),
-                          // Text(
-                          //   request.activityId,
-                          //   style: Theme.of(context).textTheme.labelSmall,
-                          // ),
-                        ],
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
